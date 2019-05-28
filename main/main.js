e2328e7da47d9c87e2904fbba11998e14dc5c65b
fann <2211256376@qq.com>
2019-05-28 12:02:13 +0800

    提交JavaScript第一题代码

--- main/main.js
+++ main/main.js
@@ -1,4 +1,11 @@
 // Write your cade below:
 module.exports = function main() {
+	use strict"
+
+	var a = prompt("请输入第一个数字:");
+	var b = prompt("请输入第二个数字:");
+
+	var result = a%b;
+	alert("结果是:"+result);
   
 };
