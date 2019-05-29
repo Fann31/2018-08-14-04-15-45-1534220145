40b967a78c279c3078c4bcf4b098b50f804dd974
fann <2211256376@qq.com>
2019-05-29 13:41:27 +0800

    求余数

--- main/main.js
+++ main/main.js
@@ -1,11 +1,6 @@
 // Write your cade below:
-module.exports = function main() {
-	use strict"
-
-	var a = prompt("请输入第一个数字:");
-	var b = prompt("请输入第二个数字:");
-
-	var result = a%b;
-	alert("结果是:"+result);
-  
+module.exports = function main(a,b) {
+    "use strict"
+    var result = a%b;
+    return result;
 };
